acb55e4ea0fe309c8070ff3b93d9b413fba1fc76
Tiago Costa <tiagodpv@gmail.com>
2022-06-18 17:43:05 -0400

    include roads

--- Delivery_Driver/Assets/Delivery.cs
+++ Delivery_Driver/Assets/Delivery.cs
@@ -4,14 +4,42 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+    [SerializeField] float destroyDelay = 0.5f;
+    bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Hit something");
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Triggered something");
+        if (other.tag == "Package" && !hasPackage)
+        {
+            Debug.Log("Package picked up");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
+            
+        }
+
+        if (other.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("Delivered to the costumer");
+            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
+
+        } 
 
     }
 }
--- Delivery_Driver/Assets/Driver.cs
+++ Delivery_Driver/Assets/Driver.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 0.1f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 15f;
+    [SerializeField] float slowSpeed = 5f;
+    [SerializeField] float boostSpeed = 30f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+  
     // Update is called once per frame
     void Update()
     {
@@ -22,4 +19,22 @@ public class Driver : MonoBehaviour
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        moveSpeed = slowSpeed;
+        Debug.Log("You are bumped");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       if(other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+            Debug.Log("You are boosted");
+        }
+        
+    }
+
+    
 }
